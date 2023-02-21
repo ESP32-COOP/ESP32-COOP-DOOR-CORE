8f467b1086f8304e7f188048485298193bc000cb
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2023-02-21 15:39:01 +0000

    start core struct

---
+++ core/core.ino
@@ -0,0 +1,30 @@
+const unsigned MODE_MAIN =  1;
+const unsigned MODE_CONNECTED 2;
+
+const unsigned DOOR_OPEN = 0;
+const unsigned DOOR_CLOSE = 1;
+
+const unsigned DOOR_MODE_OPEN = 0;
+const unsigned DOOR_MODE_CLOSE = 1;
+const unsigned DOOR_MODE_AUTO = 3;
+
+unsigned MODE  = MODE_MAIN;
+
+void setup(){
+    
+
+}
+
+
+void loop(){
+    switch (MODE)
+    {
+    case MODE_MAIN:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+}
